d6593c4e09c522f681dfb3b0f674e46df1952c34
DhirajsGithub <borsedhiraj123@gmail.com>
2024-02-02 23:51:16 +0530

    sm

--- auto-resume/src/App.jsx
+++ auto-resume/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 const CLIENT_ID = "a12b9b0d5b4c0a8f85b3";
 function App() {
+  useEffect(() => {}, []);
   function loginWithGithub() {
     window.location.assign(
       "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID
