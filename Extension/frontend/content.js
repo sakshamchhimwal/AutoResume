b3d9600c9ad5cf72d6ef646a40c31f9fb5d7fc2f
Tanishq Trivedi <tanishq.trivedi27@gmail.com>
2024-02-02 23:49:28 +0530

    linkedin parser

--- Extension/frontend/content.js
+++ Extension/frontend/content.js
@@ -1,13 +1,37 @@
 // content.js
 // This script will be injected into the web page
 
-// Get all elements with data-dev attribute
-const devElements = document.querySelectorAll("[data-dev]");
+// linkedin parser
+function parser() {
+    const jobsDescriptionContainer = document.querySelector('div.jobs-description-content__text');
 
-// Convert NodeList to Array and extract text content
-const devTextContent = Array.from(devElements).map(
-  (element) => element.textContent
-);
 
-// Send the extracted content to the background script
-chrome.runtime.sendMessage({ devContent: devTextContent });
+    if (jobsDescriptionContainer) {
+        const contentInsideArticle = jobsDescriptionContainer.innerHTML;
+        const parser = new DOMParser();
+
+        const doc = parser.parseFromString(contentInsideArticle, 'text/html');
+
+        try {
+            const doc = parser.parseFromString(contentInsideArticle, 'text/html');
+            const elements = doc.querySelectorAll('span, p, li, h2, strong, ul'); // Add more element types as needed
+
+            elements.forEach(element => {
+                const textContent = element.textContent.trim();
+                console.log(textContent);
+            });
+        } catch (error) {
+            console.error('Error parsing HTML:', error);
+        }
+    } else {
+        console.log('Article with class "jobs-description__container" not found.');
+    }
+}
+
+parser();
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === 'runFunction') {
+        parser();
+    }
+});
