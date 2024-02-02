f4c3488ac9b0d45a4267c71a45e96df36c13d817
Tanishq Trivedi <tanishq.trivedi27@gmail.com>
2024-02-03 02:38:50 +0530

    extension refactored

--- Extension/frontend/popup.js
+++
@@ -1,7 +0,0 @@
-// popup.js
-document.getElementById("runParser").addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { action: "runFunction" });
-  });
-});
